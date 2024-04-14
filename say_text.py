b797b23986ba2acc913e4770898c56d98eaa3aa0
John Burns <46507384+Jburns2019@users.noreply.github.com>
2024-04-14 00:23:59 -0700

    Added cross platform support. Random chapter generation. Non html threading.

--- say_text.py
+++ say_text.py
@@ -1,11 +1,13 @@
 import pyttsx3
 import js
+import random
 import time
 
 html_accessed = True
 try:
     from pyscript import document
 except:
+    import multiprocessing
     html_accessed = False
 
 def say_text(text: str):
@@ -77,10 +79,12 @@ def valid_verse(bible: dict, verse_breakdown: list):
 
 def get_all_verses(bible: dict, elipse: list):
     verse_list = []
+    named_list = []
 
     if len(elipse) == 1 and valid_verse(bible, elipse[0]):
         book, chap, verse = elipse[0]
         verse_list.append(bible[book][chap][verse])
+        named_list = [f'{book} {chap}:{verse}: ']
     if len(elipse) > 1 and valid_verse(bible, elipse[0]) and valid_verse(bible, elipse[1]):
         book_beg, chap_beg, verse_beg = elipse[0]
         book_end, chap_end, verse_end = elipse[1]
@@ -100,6 +104,7 @@ def get_all_verses(bible: dict, elipse: list):
             print(f'[Error]: The book of {book_end} comes before the book of {book_beg}. Choose from {choices}.')
             return verse_list
         verse_list = []
+        named_list = []
 
         book_range = books[book_beg_index:book_end_index+1]
         for book in book_range:
@@ -119,43 +124,136 @@ def get_all_verses(bible: dict, elipse: list):
                 for verse in range(verse_beg_book, verse_end_book+1):
                     if str(verse) in bible[book][str(chap)]:
                         verse_list.append(bible[book][str(chap)][str(verse)])
+                        named_list.append(f'{book} {chap}:{verse}: ')
 
-    return verse_list
+    return (verse_list, named_list)
 
-def gen_output(event=None):
-    if event == None or event != None and event.key == 'Enter':
-        bible = convert_to_dictionary()
+def get_text(bible, prompt: str):
+    elipse = parse_prompt(prompt)#'Genesis 1:1-Revelation 22:21')
+    verse_list, named_list = get_all_verses(bible, elipse)
 
-        prompt = 'Ephesians 5:8-21'
-        if html_accessed:
-            input_text = document.querySelector("#english")
-            prompt = input_text.value
+    text = ''
+    speach_text = ''
+    for i in range(len(verse_list)):
+        text += named_list[i] + verse_list[i]
+        speach_text += verse_list[i]
         
+        if i < len(verse_list) - 1:
+            text += '\n'
+            speach_text += '\n'
+
+    return (text, speach_text)
+
+def output_text(text: str, speach_text: str, event):
+    if html_accessed:
+        output_div = document.querySelector("#output")
+        output_div.innerText = text
+    else:
+        print(text)
+    
+    if event != None and len(text) < 350000:
+        #https://github.com/kripken/speak.js/tree/master
+        js.speak(speach_text)
+    elif event == None:
+        sayable_outputs = []
+        for character_index in range(0, len(speach_text), 350000):
+            max_val = min(character_index + 350000, len(speach_text))
+            
+            sayable_outputs.append(speach_text[character_index:max_val])
+
+        for sayable_output in sayable_outputs:
+            say_text(sayable_output)
+
+def start_output(function, event):
+    if not html_accessed:
+        global processes
+        if not 'processes' in globals():
+            processes = []
+        
+        while len(processes) > 0:
+            process = processes.pop()
+            process.terminate()
+        
+        process = multiprocessing.Process(target=function, args=(event,))
+        process.start()
+        processes.append(process)
+    else:
+        function(event) 
+
+def start_generating_output(event=None):
+    start_output(gen_output, event)
+
+def gen_output(event):
+    if event == None or event != None and (not hasattr(event, 'key') or event.key == 'Enter' ):
         try:
-            elipse = parse_prompt(prompt)#'Genesis 1:1-Revelation 22:21')
-            verse_list = get_all_verses(bible, elipse)
-            text = '\n'.join(verse_list)
+            bible = convert_to_dictionary()
+            
+            prompt = 'Genesis 1:1'#'Ephesians 5:8-21'
+            if html_accessed:
+                input_text = document.querySelector("#english")
+                prompt = input_text.value
+
+            text, speach_text = get_text(bible, prompt)
+            text = prompt + '\n' + text
+            speach_text = prompt.replace(':', ' ').replace('-', ' to ') + '\n' + speach_text
         except:
             text = '[Error]: Prompt was not accepted.'
+            speach_text = ''
 
-        if html_accessed:
-            output_div = document.querySelector("#output")
-            output_div.innerText = text
+        output_text(text, speach_text, event)
+
+def get_next_chapter(bible, books: list, random_book_start: str, random_chapter_start: str):
+    book_end = random_book_start
+    chapter_end = str(int(random_chapter_start) + 1)
+    if not chapter_end in bible[random_book_start]:
+        if not random_book_start == 'Revelation':
+            book_end = books[books.index(random_book_start)+1]
+            chapter_end = "1"
         else:
-            print(text)
-        
-        if event != None and len(text) < 350000:
-            #https://github.com/kripken/speak.js/tree/master
-            js.speak(text)
-        elif event == None:
-            sayable_outputs = []
-            for character_index in range(0, len(text), 350000):
-                max_val = min(character_index + 350000, len(text))
-                
-                sayable_outputs.append(text[character_index:max_val])
+            chapter_end = random_chapter_start
 
-            for sayable_output in sayable_outputs:
-                say_text(sayable_output)
+    return (book_end, chapter_end)
+
+def start_generating_random_reading(event=None):
+    start_output(gen_random_reading, event)
+
+def gen_random_reading(event):
+    try:        
+        bible = convert_to_dictionary()
+
+        books = list(bible.keys())
+
+        random_book_start = random.choice(books)
+        random_chapter_start = random.choice(list(bible[random_book_start].keys()))
+        
+        book_end, chapter_end = (random_book_start, random_chapter_start)
+        verse_end = list(bible[book_end][chapter_end].keys())[-1]
+        verse_text = f'{random_book_start} {random_chapter_start}:1-{book_end} {chapter_end}:{verse_end}'
+
+        prev_length = 0
+        length = len(get_text(bible, verse_text)[0])
+        while len(get_text(bible, verse_text)[0]) < 5000 and length > prev_length:
+            book_end, chapter_end = get_next_chapter(bible, books, book_end, chapter_end)
+            verse_end = list(bible[book_end][chapter_end].keys())[-1]
+            verse_text = f'{random_book_start} {random_chapter_start}:1-{book_end} {chapter_end}:{verse_end}'
+
+            prev_length = length
+            length = len(get_text(bible, verse_text)[0])
+
+        text, speach_text = get_text(bible, verse_text)
+        text, speach_text = get_text(bible, verse_text)
+        text = verse_text + '\n' + text
+        speach_text = verse_text.replace(':', ' ').replace('-', ' to ') + '\n' + speach_text
+    except:
+        text = '[Error]: Prompt was not accepted.'
+        speach_text = ''
+    
+    output_text(text, speach_text, event)
 
 if __name__ == '__main__' and not html_accessed:
-    gen_output()
+    start_generating_random_reading()
+    print('I launched a thread.')
+    start_generating_random_reading()
+    start_generating_output()
+    # gen_output()
+    # gen_random_reading()
