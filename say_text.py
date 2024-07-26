977fcb365e9acd2f9f853daedddbd4f0e10af143
John Burns <burnsjo@oregonstate.edu>
2024-07-25 21:13:52 -0700

    Update website.

--- say_text.py
+++ say_text.py
@@ -159,9 +159,9 @@ def output_text(text: str, speach_text: str, output_location: str, event):
             button_div = document.querySelector('#' + output_location + '-dropdown')
             button_div.innerText = title
 
-            document.getElementById('#' + output_location + '-dropdown').classList.toggle('hidden')
-            # button_div.classList.toggle('plus')
-            # button_div.classList.toggle('minus')
+            # document.getElementById('#' + output_location + '-dropdown').classList.toggle('hidden')
+            button_div.classList.toggle('plus')
+            button_div.classList.toggle('minus')
 
         output_div = document.querySelector('#' + output_location + '-output')
         output_div.innerText = text
