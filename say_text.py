dce0aecc6d0c0051d43f42117b2c9131078dfbe2
John Burns <burnsjo@oregonstate.edu>
2024-07-25 23:36:08 -0700

    Impliment instead of import levenshtein distance.

--- say_text.py
+++ say_text.py
@@ -1,5 +1,6 @@
 import js
 import random
+import numpy
 
 html_accessed = True
 try:
@@ -8,7 +9,36 @@ except:
     import multiprocessing
     html_accessed = False
 
-from Levenshtein import distance
+def distance(token1, token2):
+    distances = numpy.zeros((len(token1) + 1, len(token2) + 1))
+
+    for t1 in range(len(token1) + 1):
+        distances[t1][0] = t1
+
+    for t2 in range(len(token2) + 1):
+        distances[0][t2] = t2
+        
+    a = 0
+    b = 0
+    c = 0
+    
+    for t1 in range(1, len(token1) + 1):
+        for t2 in range(1, len(token2) + 1):
+            if (token1[t1-1] == token2[t2-1]):
+                distances[t1][t2] = distances[t1 - 1][t2 - 1]
+            else:
+                a = distances[t1][t2 - 1]
+                b = distances[t1 - 1][t2]
+                c = distances[t1 - 1][t2 - 1]
+                
+                if (a <= b and a <= c):
+                    distances[t1][t2] = a + 1
+                elif (b <= a and b <= c):
+                    distances[t1][t2] = b + 1
+                else:
+                    distances[t1][t2] = c + 1
+
+    return distances[len(token1)][len(token2)]
 
 def say_text(text: str):
     import pyttsx3
