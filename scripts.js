194948029fdb6bd22157a45ac5afa775a2a6420a
John Burns <burnsjo@oregonstate.edu>
2024-07-28 16:06:36 -0700

    Update website.

--- scripts.js
+++ scripts.js
@@ -3,3 +3,11 @@ function toggle_visibility(id_name, button) {
     button.classList.toggle('plus')
     button.classList.toggle('minus')
 }
+
+function verse_selection_loaded(id_name) {
+    var h1_tag = document.querySelector(id_name)
+
+    if (h1_tag.value.indexOf(' (Loading...)') != -1) {
+        h1_tag.innerText(h1_tag.value.replace(' (Loading...)', ' (Loaded)'))
+    }
+}
